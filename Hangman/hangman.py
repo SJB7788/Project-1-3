59992654eeecb704952d456d495d78e00c550783
SJB7788 <sbj7788@gmail.com>
2022-04-06 09:45:07 -0700

    hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,12 +1,15 @@
 import random
-from words import words
+import time
 import string
+
+words = ["jaw", "era", "death", "sandwich", "curtain", "nose", "elegant", "pedestrain", "epicalyx", "influence",   
+    "uncanny", "strange", "stooge", "sunny", "house", "cream", "fountain", "stick", "license", "together", "clock",
+    "love", "time", "zimmerman", "rai", "kwantlen"]
 
 def get_valid_word(words):
     word = random.choice(words) #Random Word from this list
-    if str(word) == 'zimmerman' or 'rai':
-        print('ayo')
-        word = random.choice(words) 
+    while word == 'rai' or word == 'zimmerman': #removing zimmerman and rai from being the answer
+        word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
     return word.lower()
@@ -87,37 +90,46 @@ hang = [ """
 def hangman():
     word = get_valid_word(words)
     word_letters = list(word)
-    alphabet = set(string.ascii_lowercase)
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
+    ,'V', 'W', 'X', 'Y', 'Z']
     used_letters = set() # letters that users already guessed
     i = 1
     # user input
     while len(word_letters) > 0:
-        #hangman picture
+        # hangman picture
         print(hang[i])
-        #to make sure that the final picture is printed before it breaks
+
+        # to make sure that the final picture is printed before it breaks
         if i == 6:
             break
-
+        
         # showing letters used after the first input
-        if i > 1:    
-            print('You have used these letters: ', ' '.join(used_letters))
+        print(' '.join(alphabet))
 
         # what current word is
         word_list = [letter if letter in used_letters else '_' for letter in word]
         print("Current word : ", " ".join(word_list))
 
-        user_guess = input('Guess a letter: ').lower()
-        if user_guess in alphabet - used_letters:
-            used_letters.add(user_guess)     
+        #user input
+        user_guess = input('Guess a letter: ').upper()
+
+        #updates the list of letters left
+        if user_guess in alphabet:
+            index = alphabet.index(user_guess)
+            alphabet.remove(alphabet[index])
+            alphabet.insert(index, '#')
+            # when guessed wrong, it updates the count of i to update the hangman picture
             if user_guess not in word_letters:
                 i += 1
             if user_guess in word_letters:
                 word_letters.remove(user_guess)
         elif user_guess in used_letters:
-            print("You have alzheimer?")
+            print("Used Letter")
         else:
             print("Write something valid")
         print(" ")
+
+        time.sleep(1) #time delay by 1s
 
     print("Final word: " + word)
     play_request()
--- Hangman/words.py
+++
@@ -1,3 +0,0 @@
-words = ["jaw", "era", "death", "sandwich", "curtain", "nose", "elegant", "pedestrain", "epicalyx", "influence",   
-    "uncanny", "strange", "stooge", "sunny", "house", "cream", "fountain", "stick", "license", "together", "clock",
-    "love", "time", "zimmerman", "rai", "kwantlen"]
