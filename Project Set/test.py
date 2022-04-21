2a09462efe896c69f798c4d4c44cd3f86c9bf490
SJB7788 <sbj7788@gmail.com>
2022-04-21 10:48:16 -0700

    slime attack and stuff

--- Project Set/test.py
+++ Project Set/test.py
@@ -3,3 +3,48 @@ import time
 
 test = Image.open(r"Project Set\Slime_(Dragon_Quest).png")
 test.show()
+
+statLvl = {
+        "Helmet": 1,
+        "Chestplate": 1,
+        "Boots": 1,
+        "Sword": 1,
+        "ATK": 10,
+        "DEF": 10,
+        "Health": 100,
+        "Speed": 5,
+        "LVPoints" : 100
+    }
+
+stat = [
+        " ____________________________________________ ",
+        "|             Character Profile              |",
+        "| ▯ Helmet (LV." + str(statLvl["Helmet"]) +
+        ") + H       LV. Points: "+ str(statLvl["LVPoints"]) +"  |",
+        "| ▯ Chestplate (LV." +
+        str(statLvl["Chestplate"]) + ") + C                    |",
+        "| ▯ Boots (LV." + str(statLvl["Boots"]) +
+        ") + B                         |",
+        "| ------------------------------------------ |",
+        "| ▯ Sword (LV." + str(statLvl["Sword"]) +
+        ") + S                         |",
+        "| ------------------------------------------ |",
+        "| Stats:                                     |",
+        "|   ATK: " + str(statLvl["ATK"]) + "                                  |",
+        "|   DEF: " + str(statLvl["DEF"]) + "                                  |",
+        "|   Health: " + str(statLvl["Health"]) + "                              |",
+        "|   Speed: " + str(statLvl["Speed"]) + "                                 |",
+        "|____________________________________________|"
+    ]
+
+for i in stat:
+    print(i)
+statUpgrade = input("H: Helmet, C: Chestplate, B: Boots, S: Sword, Q: Back to fight\n").upper()
+if statUpgrade == "H":
+    statLvl["Helmet"] += 1
+    statLvl["Health"] += 50
+print(statLvl)
+for i in stat:
+    print(i)
+print(statLvl["Health"])
+print(stat[2])
--- Project Set/text rpg.py
+++ Project Set/text rpg.py
@@ -2,7 +2,7 @@ from types import new_class
 from PIL import Image
 import time
 
-
+# starting screen
 def textRPGStart():
     print(
         """
@@ -13,8 +13,8 @@ def textRPGStart():
     |____________________________________________|
     """
     )
-    userStart = input("").upper()
     while True:
+        userStart = input("").upper()
         if userStart == "P":
             print("\nStarting Game....\n")
             time.sleep(3)
@@ -25,8 +25,9 @@ def textRPGStart():
             break
         else:
             print("Invalid Input")
+            continue
 
-
+# useless text to lower expectations
 def textRPG():
     print("This game is only a tech demo and only contains the combat sequence\n")
     time.sleep(2)
@@ -34,8 +35,12 @@ def textRPG():
     time.sleep(1)
     fightSequence()
 
-
+# function name should be straightforward
 def fightSequence():
+    # open image of a slime from dragon quest
+    slimePNG = Image.open(r"Project Set\Slime_(Dragon_Quest).png")
+    slimePNG.show()
+    # mob class to make any type of mob
     class mob:
         def __init__(enemy, name, health, ATK, DEF, LVL):
             enemy.name = name
@@ -43,7 +48,7 @@ def fightSequence():
             enemy.ATK = ATK
             enemy.DEF = DEF
             enemy.LVL = LVL
-
+    # user stat lvl at lvl 1
     statLvl = {
         "Helmet": 1,
         "Chestplate": 1,
@@ -52,35 +57,42 @@ def fightSequence():
         "ATK": 10,
         "DEF": 10,
         "Health": 100,
-        "Speed": 5
+        "Speed": 5,
+        "LVPoints" : 100
     }
-
+    # stat screen in a list so I can update the string later (bugged idk whats wrong with this actually)
     stat = [
         " ____________________________________________ ",
         "|             Character Profile              |",
         "| ▯ Helmet (LV." + str(statLvl["Helmet"]) +
-        ")          LV. Points: 0     |",
+        ") + H       LV. Points: "+ str(statLvl["LVPoints"]) +"  |",
         "| ▯ Chestplate (LV." +
-        str(statLvl["Chestplate"]) + ")                        |",
+        str(statLvl["Chestplate"]) + ") + C                    |",
         "| ▯ Boots (LV." + str(statLvl["Boots"]) +
-        ")                             |",
+        ") + B                         |",
         "| ------------------------------------------ |",
         "| ▯ Sword (LV." + str(statLvl["Sword"]) +
-        ")                             |",
+        ") + S                         |",
         "| ------------------------------------------ |",
         "| Stats:                                     |",
-        "|   ATK: 10                                  |",
-        "|   DEF: 10                                  |",
-        "|   Health: 100                              |",
-        "|   Speed: 5                                 |",
+        "|   ATK: " + str(statLvl["ATK"]) + "                                  |",
+        "|   DEF: " + str(statLvl["DEF"]) + "                                  |",
+        "|   Health: " + str(statLvl["Health"]) + "                              |",
+        "|   Speed: " + str(statLvl["Speed"]) + "                                 |",
         "|____________________________________________|"
     ]
-
-    slime = mob("Slime", 20, 5, 0, 1)  # creating a new slime class
-
+    
+    slime = mob("Slime", 20, 5, 0, 1)  # creating a new mob which is a slime
+    def enemyATK():
+            print("The slime attacks!")
+            time.sleep(0.5)
+            print(f"It dealt {slime.ATK} to your health!\n")
+            statLvl["Health"] -= slime.ATK
+    # the actual fight sequence
     while True:
         time.sleep(1)
-        print(f'{slime.name} LVL.{slime.LVL} \nEnemy Health: {slime.health}\n')
+        print(f'{slime.name} LVL.{slime.LVL} \nEnemy Health: {slime.health}\n\nYour Health: {statLvl["Health"]}\n')
+        
         time.sleep(1)
         userFightInput = input(
             "You have these options:\nA(Attack), S(Stats), R(Run): ").upper()
@@ -94,16 +106,48 @@ def fightSequence():
             if slime.health == 0:
                 print("You have defeated the slime!")
                 break
+            enemyATK()
             continue
         if userFightInput == "S":
-            for i in stat:
-                print(i)
+            # i dont know why it doesnt update the stat screen. i tested it and it changes the dictionary value just not 
+            # the stat screen. might have to do with the fact that the stat screen bases off of the original dictionary
+            # not the one that is updated during the if statement sad
+
+            # now that I think about it i probably shouldve just made the user's stat into a class and updated it that way
+            # it worked for the slime fight sequence so maybe it wouldve worked with the user's stat screen
+            while True:
+                for i in stat:
+                    print(i)
+                statUpgrade = input("H: Helmet, C: Chestplate, B: Boots, S: Sword, Q: Back to fight\n").upper()
+                if statUpgrade == "H":
+                    statLvl["Helmet"] += 1
+                    statLvl["Health"] += 50
+                    continue
+                if statUpgrade == "C":
+                    statLvl["Chestplate"] += 1
+                    statLvl["DEF"] += 10
+                    continue
+                if statUpgrade == "B":
+                    statLvl["Boots"] += 1
+                    statLvl["Speed"] += 5
+                    continue
+                if statUpgrade == "S":
+                    statLvl["Sword"] += 1
+                    statLvl["ATK"] += 10
+                    continue
+                if statUpgrade == "Q":
+                    print("")
+                    break
             continue
         if userFightInput == "R":
             print("\nFight the slime wuss\n")
+            enemyATK()
             continue
         else:
             print("Invalid Input\n")
 
 
-textRPGStart()
+
+
+fightSequence()
+#textRPGStart()
