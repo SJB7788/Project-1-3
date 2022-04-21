0fad10164ad509e16750dc0c79fb61af4e2f78fc
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2022-04-20 17:46:06 -0700

    text RPG
    man

--- Project Set/text rpg.py
+++ Project Set/text rpg.py
@@ -1,26 +1,23 @@
 from types import new_class
-from PIL import Image  
-import time 
-
-
-
-
+from PIL import Image
+import time
 
 
 def textRPGStart():
-    print(     
-    """
+    print(
+        """
     ______________________________________________
     |                 Text RPG                   |
     |                                            |
     |      Play(P)                 Quit(Q)       |
     |____________________________________________|
     """
-        )
+    )
     userStart = input("").upper()
     while True:
         if userStart == "P":
-            print("Starting Game....\n")
+            print("\nStarting Game....\n")
+            time.sleep(3)
             textRPG()
             break
         if userStart == "Q":
@@ -29,23 +26,28 @@ def textRPGStart():
         else:
             print("Invalid Input")
 
+
 def textRPG():
     print("This game is only a tech demo and only contains the combat sequence\n")
+    time.sleep(2)
+    print("You have encountered a slime!\n")
     time.sleep(1)
-    print("You have encountered a slime")
-    
+    fightSequence()
+
+
 def fightSequence():
     class mob:
-        def __init__(enemy, name, health, ATK, DEF):
+        def __init__(enemy, name, health, ATK, DEF, LVL):
             enemy.name = name
             enemy.health = health
             enemy.ATK = ATK
             enemy.DEF = DEF
+            enemy.LVL = LVL
 
     statLvl = {
-        "Helmet": 1, 
-        "Chestplate": 1, 
-        "Boots": 1, 
+        "Helmet": 1,
+        "Chestplate": 1,
+        "Boots": 1,
         "Sword": 1,
         "ATK": 10,
         "DEF": 10,
@@ -56,11 +58,15 @@ def fightSequence():
     stat = [
         " ____________________________________________ ",
         "|             Character Profile              |",
-        "| ▯ Helmet (LV." + str(statLvl["Helmet"]) + ")          LV. Points: 0     |",
-        "| ▯ Chestplate (LV." + str(statLvl["Chestplate"]) + ")                        |",
-        "| ▯ Boots (LV." + str(statLvl["Boots"]) + ")                             |",
+        "| ▯ Helmet (LV." + str(statLvl["Helmet"]) +
+        ")          LV. Points: 0     |",
+        "| ▯ Chestplate (LV." +
+        str(statLvl["Chestplate"]) + ")                        |",
+        "| ▯ Boots (LV." + str(statLvl["Boots"]) +
+        ")                             |",
         "| ------------------------------------------ |",
-        "| ▯ Sword (LV." + str(statLvl["Sword"]) +")                             |",
+        "| ▯ Sword (LV." + str(statLvl["Sword"]) +
+        ")                             |",
         "| ------------------------------------------ |",
         "| Stats:                                     |",
         "|   ATK: 10                                  |",
@@ -69,18 +75,22 @@ def fightSequence():
         "|   Speed: 5                                 |",
         "|____________________________________________|"
     ]
-    
-    slime = mob("Slime", 20, 5, 0) #creating a new slime class
+
+    slime = mob("Slime", 20, 5, 0, 1)  # creating a new slime class
 
     while True:
-        userFightInput = input("You have these options:\nA(Attack), S(Stats), R(Run): ").upper()
+        time.sleep(1)
+        print(f'{slime.name} LVL.{slime.LVL} \nEnemy Health: {slime.health}\n')
+        time.sleep(1)
+        userFightInput = input(
+            "You have these options:\nA(Attack), S(Stats), R(Run): ").upper()
         if userFightInput == "A":
             time.sleep(1)
             print("\nYou hit the slime")
             time.sleep(1)
-            print("\nYou dealt " + str(statLvl["ATK"]) + " Damage to the slime")
+            print("\nYou dealt " +
+                  str(statLvl["ATK"]) + " Damage to the slime\n")
             slime.health = slime.health - statLvl["ATK"]
-            print("Slime Health: " + str(slime.health))
             if slime.health == 0:
                 print("You have defeated the slime!")
                 break
@@ -92,8 +102,8 @@ def fightSequence():
         if userFightInput == "R":
             print("\nFight the slime wuss\n")
             continue
-        else: print("Invalid Input\n")
+        else:
+            print("Invalid Input\n")
 
-    
 
-fightSequence()
+textRPGStart()
