8430803d577d1d02d546c099200f9125d5fd7568
SJB7788 <sbj7788@gmail.com>
2022-04-25 13:07:21 -0700

    password

---
+++ Password + Image Manipulation/password.py
@@ -0,0 +1,44 @@
+from cryptography.fernet import Fernet
+
+master_pwd = input("Master Password?: ")
+
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("Password + Image Manipulation\key.key", "wb") as key_file:
+        key_file.write(key)
+"""
+def load_key():
+    file = open("Password + Image Manipulation\key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+def view():
+    with open('Password + Image Manipulation\passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: " + user + " | Password: " + fer.decrypt(passw.encode()).decode())
+
+def add():
+    name = input("Username: ")
+    pwd= input("Password: ")
+
+    with open('Password + Image Manipulation\passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+while True:
+    mode = input("\nAdd new password or view existing passwords(A = Add, V = View, Q = Quit): ").upper()
+    if mode == "Q":
+        print("Good bye")
+        break
+    if mode == "V":
+        view()
+    elif mode == "A":
+        add()
+    else:
+        print("Invalid mode")
