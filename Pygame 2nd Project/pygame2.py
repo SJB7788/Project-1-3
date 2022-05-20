9161ed46a2cffc31096551a9f7e0762200c15131
SJB7788 <sbj7788@gmail.com>
2022-05-20 11:05:23 -0700

    Update pygame2.py

--- Pygame 2nd Project/pygame2.py
+++ Pygame 2nd Project/pygame2.py
@@ -1,1 +1,20 @@
 import pygame
+from sys import exit
+pygame.init()
+
+#screen/window
+WIDTH = 1000
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tutorial")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all out elements
+    # update everything
+    pygame.display.update()
+    clock.tick(60)
