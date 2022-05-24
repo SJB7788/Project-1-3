0fac02014563f8514592f5eb92904101d8c12703
SJB7788 <sbj7788@gmail.com>
2022-05-24 14:44:39 -0700

    Update pygame2.py

--- Pygame 2nd Project/pygame2.py
+++ Pygame 2nd Project/pygame2.py
@@ -9,12 +9,16 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tutorial")
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all out elements
-    # update everything
+    
+    screen.blit(test_surface, (0, 0))
+
     pygame.display.update()
     clock.tick(60)
