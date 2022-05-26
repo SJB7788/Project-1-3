6b258894781b24ae796762314cf76c94a7a666a3
SJB7788 <sbj7788@gmail.com>
2022-05-26 11:10:55 -0700

    Update pygame2.py

--- Pygame 2nd Project/pygame2.py
+++ Pygame 2nd Project/pygame2.py
@@ -13,7 +13,9 @@ test_font = pygame.font.Font('Pygame 2nd Project/font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('Pygame 2nd Project/graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('Pygame 2nd Project/graphics/ground.png').convert_alpha()
-text_surface = test_font.render("TEST", False, 'Green')
+
+score_surface = test_font.render("TEST", False, (64, 64, 64))
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('Pygame 2nd Project/graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (700, 300))
@@ -26,16 +28,35 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
+        """if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                print("collision")"""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('Jump')
+
+        if event.type == pygame.KEYUP:
+            print('key up')
+            
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (5, 5))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    screen.blit(score_surface, score_rect)
+
     snail_rect.left -= 5
     if snail_rect.right < 0:
         snail_rect.left = 800
     screen.blit(snail_surface, snail_rect)
     player_rect.left += 1
     screen.blit(player_surface, player_rect)
+
+    """if player_rect.colliderect(snail_rect):
+        print("collision")"""
+
+    """mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse_pos):
+        print(pygame.mouse.get_pressed())"""
 
     pygame.display.update()
     clock.tick(60)
