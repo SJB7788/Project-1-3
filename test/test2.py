51a0cf9d71ef5f265ef23eb061ef1df8eb9f9f9a
SJB7788 <sbj7788@gmail.com>
2022-06-17 11:16:11 -0700

    asdasd

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,8 @@
+from pydoc import describe
 import random
 from re import X
 from secrets import choice
+from tkinter import EventType
 import pygame
 from sys import exit
 from random import randint
@@ -15,21 +17,26 @@ clock = pygame.time.Clock()
 enemySPD = 5
 bullet_location = 6
 
-bullet_x = 630
-bullet_y = 200
+obstacle_pos = 660
+player_speed = 10
+
+player_x = 600
+player_y = 700
+player_bullet_x = player_x
 
 class Obstacles(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         #Enemy
-        hamza_1 = pygame.image.load('assets/hamza intensifies.png').convert_alpha() #dont forgte to cahnge it
-        hamza_2 = pygame.image.load('assets/hamza intensifies 2.png').convert_alpha()
+        hamza_1 = pygame.image.load('test/assets/hamza intensifies.png').convert_alpha() #dont forgte to cahnge it
+        hamza_2 = pygame.image.load('test/assets/hamza intensifies 2.png').convert_alpha()
         hamza_1 = pygame.transform.scale(hamza_1, (146, 123))
         hamza_2 = pygame.transform.scale(hamza_2, (146, 123))
         self.frames = [hamza_1, hamza_2]
         self.animation_index = 0
 
         self.image = self.frames[self.animation_index]
+        self.image_r = self.image.get_rect()
         self.rect = self.image.get_rect(midbottom = (600, 150))
         
 
@@ -41,7 +48,7 @@ class Obstacles(pygame.sprite.Sprite):
     
     def phase_1(self):
         #Hamza movement
-        global enemySPD
+        global enemySPD, obstacle_pos
         if self.rect.x <= 100:
             enemySPD = enemySPD * -1
             self.rect.x += enemySPD
@@ -50,6 +57,7 @@ class Obstacles(pygame.sprite.Sprite):
             self.rect.x += enemySPD
         else: 
             self.rect.x += enemySPD
+            obstacle_pos = self.rect.x + 60
 
     def update(self):
         self.phase_1()
@@ -59,65 +67,132 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         #Enemy
-        bullet = pygame.image.load('assets/pewpew.jpg').convert_alpha()
+        bullet = pygame.image.load('test/assets/pewpew.jpg').convert_alpha()
         bullet = pygame.transform.scale(bullet, (50, 20))
 
         self.image = pygame.transform.rotate(bullet, 90)
-        self.rect = self.image.get_rect(midbottom = (bullet_x, bullet_y))
-
-    def location(self):
-        #Hamza movement
-        global bullet_location, bullet_x
-        if bullet_x <= 200:
-            bullet_location *= -1
-            bullet_x = bullet_x + 100
-            print(bullet_x)
-        if bullet_x >= 1100:
-            bullet_location *= -1
-            bullet_x = bullet_x - 170
-            print(bullet_x)
-        else: 
-            bullet_x += bullet_location
+        self.rect = self.image.get_rect(midbottom = (obstacle_pos, 200))
 
     def destroy(self):
         if self.rect.y >= 850:
             self.kill()
         
     def update(self):
-        self.location()
-        self.rect.y += 20
+        self.rect.y += 10
         self.destroy()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         #player image
+        xerath = pygame.image.load('test/assets/xerath.png')
+        xerath_2 = pygame.image.load('test/assets/xerath 2.png')
+        xerath = pygame.transform.rotate(xerath, 180)
+        xerath_2 = pygame.transform.rotate(xerath_2, 180)
+        xerath = pygame.transform.scale(xerath, (60, 100))
+        xerath_2 = pygame.transform.scale(xerath_2, (60, 100))
         
+        self.frames = [xerath, xerath_2]
+        self.animation_index = 0
+
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (600, 700))
+        
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+    
+    def controls(self):
+        global player_speed
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w] and self.rect.y >= 0: #up
+            self.rect.y -= player_speed
+
+        if keys[pygame.K_s] and self.rect.y <= 610: #down
+            self.rect.y += player_speed
+            
+        if keys[pygame.K_d] and self.rect.x <= 1090: #right
+            self.rect.x += player_speed
+                
+        if keys[pygame.K_a] and self.rect.x >= 0: #left
+            self.rect.x -= player_speed
+
+
+    def update(self):
+        global player_x, player_y
+        self.animation_state()
+        self.controls()
+        player_x = self.rect.x
+        player_y = self.rect.y
+
+class Player_bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        bullet = pygame.image.load('test/assets/pewpew player.png')
+        bullet = pygame.transform.scale(bullet, (50, 20))
+        bullet = pygame.transform.rotate(bullet, 90)
+
+        self.image = bullet
+        self.image_r = self.image.get_rect()
+        self.rect = self.image.get_rect(midbottom = (player_x + 30, 650))
+
+    def destroy(self):
+        col = self.rect.colliderect(ob.image_r)
+        print(col)
+    
+    def update(self):
+        self.destroy()
+        self.rect.y -= 20
+
+
 
 obstacle_group = pygame.sprite.Group()
-obstacle_group.add(Obstacles(), Bullet())
+obstacle_group.add(Obstacles())
+
+player_group = pygame.sprite.Group()
+player_group.add(Player())
+
+p_bullet = Player_bullet()
+ob = Obstacles()
+
+sprite = pygame.sprite.Group()
+
 
 game_active = True
 
 #timer
 bullet_timer = pygame.USEREVENT +1
-pygame.time.set_timer(bullet_timer, 800)
+pygame.time.set_timer(bullet_timer, 400)
+
+player_bullet_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(player_bullet_timer, 800)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
         if event.type == bullet_timer:
             obstacle_group.add(Bullet())
+        
+        if event.type == player_bullet_timer:
+            player_group.add(Player_bullet())
     
     if game_active:
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, 1200, 800))
-        
+
         obstacle_group.draw(screen)
         obstacle_group.update()
-    """bullet_group.draw(screen)
-    bullet_group.update()
-"""
+
+        player_group.draw(screen)
+        player_group.update()
+
+        for player_group in pygame.sprite.groupcollide(obstacle_group, player_group, False, False):
+            print(True)
+
     pygame.display.update()
     clock.tick(60)
